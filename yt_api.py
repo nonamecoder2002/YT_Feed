8586f4cebb52c4196eeb2c68f8075e442d66dad9
nonamecoder2002 <alex2002uz@gmail.com>
2020-08-07 19:26:29 +0300

    YT_API Stage Done!

--- yt_api.py
+++ yt_api.py
@@ -4,18 +4,17 @@ api_key = 'AIzaSyCiqyUzl_r7ZJvmcuf1HmMnLVIapwTDWlg'
 
 youtube = build(serviceName='youtube', version='v3', developerKey=api_key)
 
-channel_id_pool = [
-    'UCGBFIBxDuaEmrL5CxXSxx6g',
-    'UCptRK95GEDXvJGOQIFg50fg',
-    'UC9MK8SybZcrHR3CUV4NMy2g',
-    'UCPIKvg4P2pRDdmyN1gi9bnA',
-    'UCg0bIYJXvberMBRdO1k1Dxg',
-    'UC1qWaT8_iPHSBYgB4T2ltuA',
-    'UCCezIgC97PvUuR4_gbFUs5g',
-    'UCq7JZ8ATgQWeu6sDM1czjhg',
-    'UC9DXAsBD4-ITVuvpd68401Q',
-    'UCspfe9lef7ApJaHQsOcPC1A'
-
+channel_pool = [
+    {'id': 'UCGBFIBxDuaEmrL5CxXSxx6g', 'name': 'СТАС'},
+    {'id': 'UCptRK95GEDXvJGOQIFg50fg', 'name': 'Игорь Линк'},
+    {'id': 'UC9MK8SybZcrHR3CUV4NMy2g', 'name': 'Диджитализируй!'},
+    {'id': 'UCPIKvg4P2pRDdmyN1gi9bnA', 'name': 'MovieMaker'},
+    {'id': 'UCg0bIYJXvberMBRdO1k1Dxg', 'name': 'Izzy ᴸᴬᴵᶠ'},
+    {'id': 'UC1qWaT8_iPHSBYgB4T2ltuA', 'name': 'Ай, Как Просто!'},
+    {'id': 'UCCezIgC97PvUuR4_gbFUs5g', 'name': 'Corey Schafer'},
+    {'id': 'UCq7JZ8ATgQWeu6sDM1czjhg', 'name': 'StopGame.Ru'},
+    {'id': 'UC9DXAsBD4-ITVuvpd68401Q', 'name': 'AutoTopNL'},
+    {'id': 'UCspfe9lef7ApJaHQsOcPC1A', 'name': 'overbafer1'}
 ]
 
 # channel_name_pool = []
@@ -57,8 +56,8 @@ def parse_snippet(snippet: dict):
     return parse_result
 
 
-for channelId in channel_id_pool:
-    respond_item = search_latest(channel_id=channelId)['items'][0]
+for channelId in channel_pool:
+    respond_item = search_latest(channel_id=channelId['id'])['items'][0]
     v_link = video_link(item=respond_item)
     video_dict = parse_snippet(snippet=respond_item['snippet'])
     print(video_dict['channel_name'], '\n\t', video_dict['title'], '\n', video_dict['preview'], '\n\n', v_link, '\n')
